8418dfdada2cc13de073b92cf61fb90de539b6b7
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-16 14:50:33 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("The size of a char: %c byte(s)", sizeof(char));
-       	printf("The size of an int: %d byte(s)", sizeof(int));
-	printf("The size of a long int: %ld byte(s)", sizeof(long int));
-	printf("The size of a long long int: %lld byte(s)", sizeof(long long int));
+	printf("The size of a char: %c byte(s)\n", sizeof(char));
+	printf("The size of an int: %d byte(s)\n", sizeof(int));
+	printf("The size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("The size of a long long int: %lld byte(s)\n", sizeof(long long int));
 	printf("The size of a float: %f byte(s)", sizeof(float));
 	return (0);
 }
