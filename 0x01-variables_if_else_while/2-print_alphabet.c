788840d201202288272bc2c403eace71b12c19e2
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-17 14:54:01 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar (c);
+		putchar ('\n');
+	}
+	return (0);
+}
