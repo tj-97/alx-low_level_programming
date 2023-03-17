7fbf4f41c56fc01c60b5af09579c8598b24ec943
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-17 16:43:05 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b = (char)',';
+	int c = (char)' ';
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar ('0' + a);
+		putchar (b);
+		putchar (c);
+	}
+	putchar ('\n');
+	return (0);
+}
