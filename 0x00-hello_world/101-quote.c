f8c27e55cb6193aabe2e77cdb8cd0cec18e1669d
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-19 05:08:12 -0700

    Modified script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19 ";
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	fprintf(stdout, "%s", quote);
+	fprintf(stdout, "%s\n", quote);
 	return (1);
 }
