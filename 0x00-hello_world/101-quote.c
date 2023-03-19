5987057f946e14517908c52bcd07e1bb6311ba83
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-19 05:02:24 -0700

    Write C program to print quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stdout, "%s", quote);
+	return (1);
+}
