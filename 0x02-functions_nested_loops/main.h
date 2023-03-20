70e32baaad635202589bbb62dee403d83b68e929
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-20 06:21:56 -0700

    Completing task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,7 +13,24 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
+
+/** printchar - write the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1
+ */
 int printchar (char* c)
 {
 	printf("%s\n", c);
+}
+
+int print_alphabet()
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar (c);
+	}
+	_putchar ('\n');
 }
