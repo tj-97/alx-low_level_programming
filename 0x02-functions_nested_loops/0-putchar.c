02c08f5fc25a5b73c97e6998baec51979d05bb04
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-20 06:04:55 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * call _putchar
+ * print "_putchar"
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c[8] = "_putchar";
+	
+	printchar (c);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This readme is for projecte 0x02-functions, nested, loops
