e546508c60d21de1e55b02d3e1e6f30d0d3246f0
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-21 10:59:40 -0700

    Completing task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @i: the int to check
+ * Return: the absolute value of the in
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	return (-i);
+}
