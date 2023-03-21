1d9a885b7701bdb1b2f31a5b2f19f0d5860fbb74
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-21 12:25:51 -0700

    Completing task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,6 +3,7 @@
 
 /**
  * print_last_digit - prints the last digit of a number
+ * @i: integer to be checked
  * Return: the value of the last digit
  */
 int print_last_digit(int i)
@@ -11,7 +12,7 @@ int print_last_digit(int i)
 
 	last_digit = i % 10;
 	if (last_digit < 0)
-		last_digit = last_digit* -1;
+		last_digit = last_digit * -1;
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
