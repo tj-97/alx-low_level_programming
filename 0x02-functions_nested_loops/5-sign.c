ce8b9204cd030b06a28d9b3394530c70e6e51c53
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-21 10:22:08 -0700

    Completing task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,9 @@
 /**
 * print_sign - prints the sign of a number
 * @n: the number to be checked
-* Return: 1 and prints + if n is greater than zero
-* Return: 0 and prints 0 if n is zero
-* Return: -1 and prints - if n is less than zero
+* Return: 1 and prints + if n is greater than zero,
+* 0 and prints 0 if n is zero,
+* and prints - if n is less than zero
 */
 int print_sign(int n)
 {
@@ -17,8 +17,6 @@ int print_sign(int n)
 		_putchar (45);
 		return (-1);
 	} else
-	{
 		_putchar ('0');
 		return (0);
-	}
 }
