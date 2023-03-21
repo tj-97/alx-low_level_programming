f0247faba1d497dfddaac7093793641f0cf5e714
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-21 09:26:11 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int _islower(int c) - checks if character is lowercase
+* int _islower(int c) - checks if character is lowercase
 * @c - character to check
 * return on success 1
 * return on failure 0
@@ -12,7 +12,5 @@ int _islower(int c)
 	if (islower(c))
 	{
 		return (1);
-	} else {
-		return (0);
 	}
 }
