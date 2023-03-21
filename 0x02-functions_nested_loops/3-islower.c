90a47cb1b967267a70c12f4d6bdeb33626d1ae33
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-21 09:22:37 -0700

    Completed task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * int _islower (int c) - checks if character is lowercase
+* @c - character to check
+* return on success 1
+* return on failure 0
+*/
+int _islower(int c)
+{
+	int islower(int c);
+
+	if (islower(c))
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
