8de1ab3d1fd4ed5245d0f857537646ccdebec9a2
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-21 20:35:41 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns
+ * the result
+ * @a: integer to be added to b
+ * @b: integer to be added to a
+ * Result: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
