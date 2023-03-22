05b673bb33142d62fc5a1c8941cab67dd092c549
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-21 21:10:40 -0700

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * add - adds two integers and returns
- * the result
- * @a: integer to be added to b
- * @b: integer to be added to a
- * Result - sum of a and b
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
  */
 
 int add(int a, int b)
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all natural numbers
+ * from n to 98
+ * @n: number passed to function
+ */
+void print_to_98(int n)
+{
+	int a;
+
+	if (a = n; a <= 98; a++)
+	{
+		_putchar (a + '0');
+		_putchar (',');
+		_putchar (' ');
+	}
+
+	if (a = n; a > 98 && a != 98; a--)
+	{
+		_putchar (a + '0');
+		_putchar (',');
+		_putchar (' ');
+	}
+	_putchar ('\n');
+}
