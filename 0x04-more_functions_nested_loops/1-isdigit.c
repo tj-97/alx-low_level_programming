db9fa6bcc243b2545e8725a408e339f896077ad3
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-23 04:28:27 -0700

    Added files for task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isdigit - checks for a digit (0 - 9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+		return (1);
+	else
+		return (0);
+}
