39331edb341e89e9d3168a3cf39feb76a94f2cd1
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-23 00:53:11 -0700

    Added files for task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - checks for uppercase character
+ * @c: char to be checked
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	int isupper(int);
+
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
