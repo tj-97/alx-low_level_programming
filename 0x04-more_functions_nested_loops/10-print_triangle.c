0ed13c157b413a1f34da2f42bb496fde45b3b901
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-24 04:48:32 -0700

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,9 +13,9 @@ void print_triangle(int size)
 	{
 		int x, y;
 
-		for (y = 0; y <= size; y++)
+		for (y = 1; y <= size; y++)
 		{
-			for (x = size; x >= 0; x--)
+			for (x = size; x >= 1; x--)
 			{
 				if (x > y)
 					_putchar (' ');
