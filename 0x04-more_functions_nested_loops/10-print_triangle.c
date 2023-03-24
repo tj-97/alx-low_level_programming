076339df010d4399f4afb20e04b04e0c703938e9
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-24 04:44:55 -0700

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size - size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+		_putchar ('\n');
+	else
+	{
+		int x, y;
+
+		for (y = 0; y <= size; y++)
+		{
+			for (x = size; x >= 0; x--)
+			{
+				if (x > y)
+					_putchar (' ');
+				else
+					_putchar (35);
+			}
+			_putchar ('\n');
+		}
+	}
+}
