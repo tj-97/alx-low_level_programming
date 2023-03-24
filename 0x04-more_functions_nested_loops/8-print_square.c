5206f8d56e08179ce06ae0ca5e8de04e15486712
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-24 02:27:39 -0700

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		int x, y;
+
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y <= size; y++)
+			{
+				_putchar (35);
+			}
+			_putchar ('\n');
+		}
+	}
+
+}
