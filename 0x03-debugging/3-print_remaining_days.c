687039973ecf80189d4c6c5df67cbc7a92d922e7
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-24 11:57:22 -0700

    3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month >= 2 && day >= 28)
 		{
 			day++;
 		}
@@ -24,14 +24,14 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 3 && day == 61)
+		if (month == 1 && day == 59)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
+			printf("Remaining days: %d\n", 366 - day);
 		}
 	}
 }
