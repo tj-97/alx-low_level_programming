651b75f6e0c1735b2b0a281bffe6f9381c5811cd
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-28 11:00:41 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * followed by a new line
+ * @a: array of integers
+ * @n: elements to be printed
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
+
+	if (i == (n - 1))
+	{
+		printf("%d", a[n - 1]);
+	}
+
+	printf("\n");
+}
