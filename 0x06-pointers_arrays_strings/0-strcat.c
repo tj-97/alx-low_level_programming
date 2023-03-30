d71f9771a731805ac01a729f2c2b93e2f5e99138
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-30 04:00:13 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: output string
+ * @src: input string
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = dest[j];
+		i++;
+		j++;
+	}
+	
+	dest[i] = '\0';
+	return (dest);
+}
