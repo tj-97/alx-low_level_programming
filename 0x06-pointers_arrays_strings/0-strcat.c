829252eeff43ef8df9103929b90d2ee65b4390e7
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-30 04:11:16 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strcat - concatenates two strings
@@ -8,22 +9,10 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	char concat[100];
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = dest[j];
-		i++;
-		j++;
-	}
+	strcpy(concat, dest);
+	strcpy(concat, src);
 	
-	dest[i] = '\0';
 	return (dest);
 }
