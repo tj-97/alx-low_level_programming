c338428bd86f5ac5361f2d89ed971dbbcb516a32
Tatum Noble <87891499+tj-97@users.noreply.github.com>
2023-03-30 08:42:53 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @ch: character to capitalize
+ * Return: capital character
+ */
+
+char *cap_string(char *ch)
+{
+	int i;
+
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if ((ch[i] >= 'a' && ch[i] <= 'z'))
+			{
+				ch[i] = ch[i] - 32;
+				continue;
+			}
+		}
+		if (ch[i] == ' ')
+		{
+			++i;
+			if (ch[i] >= 'a' && ch[i] <= 'z')
+			{
+				ch[i] = ch[i] - 32;
+				continue;
+			}
+		}
+		if (ch[i] == '.')
+		{
+			++i;
+			if (ch[i] >= 'a' && ch[i] <= 'z')
+			{
+				ch[i] = ch[i] - 32;
+				continue;
+			}
+		}
+		if (ch[i] == '	')
+		{
+			++i;
+			if (ch[i] >= 'a' && ch[i] <= 'z')
+			{
+				ch[i] = ch[i] - 32;
+				continue;
+			}
+		}
+		if (ch[i] == '\n')
+		{
+			++i;
+			if (ch[i] >= 'a' && ch[i] <= 'z')
+			{
+				ch[i] = ch[i] - 32;
+				continue;
+			}
+		}
+	}
+
+	return (ch);
+}
